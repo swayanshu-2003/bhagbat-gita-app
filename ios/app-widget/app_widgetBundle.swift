b41258d7a2d7dd73424cbe85d4521caffe9d342a
swayanshu-2003 <swayanshupanda650@gmail.com>
2025-10-02 17:32:56 +0000

    fix

---
+++ ios/app-widget/app_widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  app_widgetBundle.swift
+//  app-widget
+//
+//  Created by ec2-user on 10/2/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct app_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        app_widget()
+        app_widgetControl()
+    }
+}
